ace8fad53dc0c26d377e93461cb4d3999065bda6
Patrick Adesso <patrickadesso@gmail.com>
2017-07-08 11:33:11 -0400

    UI Refactoring on the visualizer panel.

---
+++ SimpleSamplerWPF/SimpleSamplerWPF/ViewModel/VisualizerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+using SimpleSamplerWPF.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSamplerWPF.ViewModel
+{
+    public class VisualizerViewModel : ViewModelBase
+    {
+        private CachedSound sample;
+
+        public VisualizerViewModel()
+        {
+          
+        }
+    }
+}
