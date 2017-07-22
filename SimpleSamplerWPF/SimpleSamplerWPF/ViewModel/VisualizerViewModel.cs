4b752542ad95bb15b5fd7cbd2625477f8f80ebeb
Patrick Adesso <patrickadesso@gmail.com>
2017-07-21 22:36:02 -0400

    Basic binding of points to polyline in visualizer.

--- SimpleSamplerWPF/SimpleSamplerWPF/ViewModel/VisualizerViewModel.cs
+++ SimpleSamplerWPF/SimpleSamplerWPF/ViewModel/VisualizerViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
+using NAudio.Wave;
 using SimpleSamplerWPF.Helpers;
 using SimpleSamplerWPF.Helpers.Events;
 using SimpleSamplerWPF.Logic;
@@ -10,6 +11,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 // Visualizer converted to WPF and MVVM based on:
 // http://www.giawa.com/tutorials/
@@ -19,15 +23,26 @@ namespace SimpleSamplerWPF.ViewModel
 {
     public class VisualizerViewModel : ViewModelBase
     {
+        WaveStream waveStream;
         private Sample selectedSample;
 
         public RelayCommand PlaySampleCommand { get; private set; }
+
+        PointCollection polylinePoints;
 
         public VisualizerViewModel()
         {           
             PlaySampleCommand = new RelayCommand(PlaySample, IsSampleLoaded);
 
             Messenger.Default.Register<Sample>(this, m => SelectedSample = m);
+
+            //TEST DATA
+            polylinePoints = new PointCollection();
+            polylinePoints.Add(new Point(10, 100));
+            polylinePoints.Add(new Point(100, 200));
+            polylinePoints.Add(new Point(200, 30));
+            polylinePoints.Add(new Point(250, 200));
+            polylinePoints.Add(new Point(200, 150));
         }
 
         /// <summary>
@@ -58,7 +73,20 @@ namespace SimpleSamplerWPF.ViewModel
 
             set
             {
-                Set("Sample", ref selectedSample, value);
+                Set("SelectedSample", ref selectedSample, value);
+            }
+        }
+
+        public PointCollection PolylinePoints
+        {
+            get
+            {
+                return polylinePoints;
+            }
+
+            set
+            {
+                Set("PolylinePoints", ref polylinePoints, value);
             }
         }
     }
